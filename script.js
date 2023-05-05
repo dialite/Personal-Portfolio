5c42717e5c46d32e6a84771ac05167f8c65f1feb
dialite <raymondolisa01@gmail.com>
2023-05-05 21:41:04 +0100

    Correct local storage call of empty inputs

--- script.js
+++ script.js
@@ -345,31 +345,42 @@ const userEmail = document.getElementById("email");
 const userMessage = document.getElementById("message");
 
 // Object to store user data
-const userData = {
-  nameInput: userName,
-  emailInput: userEmail,
-  messageInput: userMessage,
+let userData = {
+  nameInput: "",
+  emailInput: "",
+  messageInput: "",
 };
+
+// Retrieve data from localStorage
+const savedUserData = localStorage.getItem("userData");
+if (savedUserData) {
+  userData = JSON.parse(savedUserData);
+}
+
+// Set input field values from userData
+userName.value = userData.nameInput;
+userEmail.value = userData.emailInput;
+userMessage.value = userData.messageInput;
 
 // Store data using localStorage
 function storeData() {
-  userData.nameInput = userName.value;
-  userData.emailInput = userEmail.value;
-  userData.messageInput = userMessage.value;
+  // Only store non-empty values
+  if (userName.value !== "") {
+    userData.nameInput = userName.value;
+  }
+  if (userEmail.value !== "") {
+    userData.emailInput = userEmail.value;
+  }
+  if (userMessage.value !== "") {
+    userData.messageInput = userMessage.value;
+  }
   localStorage.setItem("userData", JSON.stringify(userData));
 }
 
-// call storeData at every event
+// Call storeData at every event
 userName.addEventListener("keyup", storeData);
 userEmail.addEventListener("keyup", storeData);
 userMessage.addEventListener("keyup", storeData);
-
-// Check for info in local-storage
-const savedUserData = localStorage.getItem("userData");
-document.getElementById("name").value = JSON.parse(savedUserData).nameInput;
-document.getElementById("email").value = JSON.parse(savedUserData).emailInput;
-document.getElementById("message").value =
-  JSON.parse(savedUserData).messageInput;
 
 Array.from(projectBtns).forEach((projectBtn) =>
   projectBtn.addEventListener("click", (e) => {
